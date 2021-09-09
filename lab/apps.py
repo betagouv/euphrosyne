f30baa42f52f53083e86e6562036f816c73d03a9
lajarre <alexandre.hajjar@gmail.com>
2021-09-09 20:34:11 +0200

    Add black config & reformat some files

--- lab/apps.py
+++ lab/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LabConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lab'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "lab"
