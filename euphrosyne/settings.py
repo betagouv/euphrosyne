767db675df96fe7cd83b3b624c2676aba008b07e
lajarre <alexandre.hajjar@gmail.com>
2021-09-09 18:53:13 +0200

    Depend on env vars for debug and secret key

--- euphrosyne/settings.py
+++ euphrosyne/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--(s52f*-94=1&m=bsa(ra_omefte65t#!@&9pdj*ey&icgpfx#"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DJANGO_DEBUG", False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
