8fe9ac898b03d7e67bae284c07f1d6ccd848f949
lajarre <alexandre.hajjar@gmail.com>
2021-09-09 20:25:49 +0200

    Fix pylint errors

--- euphrosyne/settings.py
+++ euphrosyne/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DJANGO_DEBUG", False))
+DEBUG = bool(os.getenv("DJANGO_DEBUG", ""))
 
 ALLOWED_HOSTS = ["*"]
 
