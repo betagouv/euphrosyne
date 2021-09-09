cb6e18134b04659d11be501b1c1ffe7558a34f36
lajarre <alexandre.hajjar@gmail.com>
2021-09-09 16:13:03 +0200

    Use python-dotenv to load env vars

--- euphrosyne/settings.py
+++ euphrosyne/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import dotenv  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +75,6 @@ WSGI_APPLICATION = 'euphrosyne.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "USER": os.getenv("DB_USER"),
