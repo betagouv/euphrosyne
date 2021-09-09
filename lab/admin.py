499a97d450872688e237a74311a4f246997c41cf
lajarre <alexandre.hajjar@gmail.com>
2021-09-09 18:45:33 +0200

    Improve Experiment model & add admin

--- lab/admin.py
+++ lab/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Experiment)
+class ExperimentAdmin(admin.ModelAdmin):
+    list_display = ["name", "date", "particle_type", "modified", "created"]
--- lab/models.py
+++ lab/models.py
@@ -2,7 +2,18 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _, ugettext
 
 
-class Experiement(models.Model):
+class Experiment(models.Model):
+    created = models.DateTimeField(
+        _("Created"),
+        auto_now_add=True,
+        help_text=_("Date this entry was first created"),
+    )
+    modified = models.DateTimeField(
+        _("Modified"),
+        auto_now=True,
+        help_text=_("Date this entry was most recently changed."),
+    )
+
     OPEN_STATUS = 1
     CLOSED_STATUS = 2
     STATUS_CHOICES = (
@@ -15,13 +26,23 @@ class Experiement(models.Model):
         default=OPEN_STATUS,
     )
 
-    created = models.DateTimeField(
-        _("Created"),
+    name = models.CharField(_("Experiment name"), max_length=255, unique=True)
+
+    date = models.DateTimeField(
+        _("Experiment date"),
         blank=True,
-        help_text=_("Date this ticket was first created"),
+        help_text=_("Date of the experiment."),
     )
-    modified = models.DateTimeField(
-        _("Modified"),
-        blank=True,
-        help_text=_("Date this ticket was most recently changed."),
+
+    PROTON_PARTICLE = 1
+    ALPHA_PARTICLE = 2
+    DEUTON_PARTICLE = 3
+    PARTICLES_CHOICES = (
+        (PROTON_PARTICLE, _("Proton")),
+        (ALPHA_PARTICLE, _("Alpha particle")),
+        (DEUTON_PARTICLE, _("Deuton")),
+    )
+    particle_type = models.IntegerField(
+        _("Particle type"),
+        choices=PARTICLES_CHOICES,
     )
