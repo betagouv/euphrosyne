e3b743ea5094acd3903f2993a46748eeeecd571c
lajarre <alexandre.hajjar@gmail.com>
2021-09-10 10:22:47 +0200

    Create test GA workflow

--- manage.py
+++ manage.py
@@ -10,6 +10,7 @@ def main():
     dotenv.load_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "euphrosyne.settings")
     try:
+        # pylint: disable=import-outside-toplevel
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
