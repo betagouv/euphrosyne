3a33d17201c65c3861ad9a7b37e77ed042e1bc10
lajarre <alexandre.hajjar@gmail.com>
2021-09-16 12:36:54 +0200

    Use whitenoise for serving static files

--- euphrosyne/settings.py
+++ euphrosyne/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -132,6 +133,9 @@ LOCALE_PATHS = ["locale"]
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "_static")
+
+if not DEBUG:
+    STATICFILES_STORAGE = "storage.CustomCompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
