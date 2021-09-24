095dab6ec82e8e6fb9a9522619a1362ef177dd7c
lajarre <alexandre.hajjar@gmail.com>
2021-09-24 13:38:51 +0200

    Add stock reset password views

--- euphrosyne/urls.py
+++ euphrosyne/urls.py
@@ -14,10 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 import debug_toolbar
 
 urlpatterns = [
+    path(
+        "admin/password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
     path("admin/", admin.site.urls),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ] + ([path("__debug__/", include(debug_toolbar.urls))] if settings.DEBUG else [])
