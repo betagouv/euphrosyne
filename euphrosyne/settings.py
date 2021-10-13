b88d4059bc84b6ac5523e60ffc8fbbacd3acbc94
lajarre <alexandre.hajjar@gmail.com>
2021-10-13 16:43:52 +0200

    Allow DEBUG on scalingo

--- euphrosyne/settings.py
+++ euphrosyne/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DJANGO_DEBUG", ""))
 
-ALLOWED_HOSTS = ["localhost", ".scalingo.io"] if not DEBUG else []
+ALLOWED_HOSTS = ["localhost", ".scalingo.io"]
 
 SITE_URL = os.environ["SITE_URL"]
 
@@ -182,7 +182,7 @@ AUTH_USER_MODEL = "euphro_auth.User"
 
 # For Django Debug Toolbar
 
-INTERNAL_IPS = ["127.0.0.1"]
+INTERNAL_IPS = ["127.0.0.1", "91.121.134.174"]
 
 # Sending emails
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
