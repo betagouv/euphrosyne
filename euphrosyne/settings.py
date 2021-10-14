6df22b8b54244375da2e9e4aadc51f009ea08926
lajarre <alexandre.hajjar@gmail.com>
2021-10-14 16:47:29 +0200

    ⚙️ Allow defining ALLOWED_HOSTS in environment var

--- euphrosyne/settings.py
+++ euphrosyne/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DJANGO_DEBUG", ""))
 
-ALLOWED_HOSTS = ["localhost", ".scalingo.io"] if not DEBUG else []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split() or (
+    ["localhost", ".scalingo.io"] if not DEBUG else []
+)
 
 SITE_URL = os.environ["SITE_URL"]
 
