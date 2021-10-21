e7654d07c919209ac857061aed610156767a0f3b
witold <witoldwrob@gmail.com>
2021-10-21 13:15:55 +0200

    adds global style, following State guildelines
    updates translations
    
    fixes template issue

--- lab/admin/participation.py
+++ lab/admin/participation.py
@@ -5,13 +5,15 @@ from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 
+from shared.admin import ModelAdmin
+
 from ..lib import is_lab_admin
 from ..models import Participation, Project
 
 
 # Allowance: ADMIN:lab admin, EDITOR:project leader, VIEWER:<custom (obj.user)>
 @admin.register(Participation)
-class ParticipationAdmin(admin.ModelAdmin):
+class ParticipationAdmin(ModelAdmin):
     list_display = ("project", "user")
     readonly_fields = ("project", "user")
 
