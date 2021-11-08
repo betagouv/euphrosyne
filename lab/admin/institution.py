d80d9e30ce36c14174c7f02d5aee025818cefec6
witold <witoldwrob@gmail.com>
2021-11-08 14:08:21 +0100

    overrides admin base template to add our base style

--- lab/admin/institution.py
+++ lab/admin/institution.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
-from shared.admin import ModelAdmin
-
 from ..models import Institution
 
 
 # Allowance: ADMIN:lab admin, EDITOR:lab admin, CREATOR: any user, VIEWER:any user
 @admin.register(Institution)
-class InstitutionAdmin(ModelAdmin):
+class InstitutionAdmin(admin.ModelAdmin):
     list_display = ("name", "country")
--- shared/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-
-class ModelAdmin(admin.ModelAdmin):
-    class Media:
-        css = {"all": ("css/euphro_admin/base.css",)}
