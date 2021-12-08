a553ab3c65d976d56da8d5b3d8fbbf946f95a791
witold <witoldwrob@gmail.com>
2021-12-08 10:36:17 +0100

    improves objectgoup form tests

--- lab/tests/forms/object/test_objectgroup_form.py
+++ lab/tests/forms/object/test_objectgroup_form.py
@@ -1,6 +1,7 @@
-from lab.models.run import ObjectGroup
+from unittest.mock import MagicMock
 
 from ....forms import ObjectGroupAddChoices, ObjectGroupForm
+from ....models import ObjectGroup
 
 
 def test_empty_label_group_for_object_group_raise_validation():
@@ -27,14 +28,50 @@ def test_empty_label_group_for_single_object_is_allowed():
     assert not form.has_error("label", code="label-required-for-mulitple-objectss")
 
 
-def test_update_object_has_disabled_add_type_field():
+def test_update_object_disables_add_type_field():
+    instance = MagicMock(side_effect=ObjectGroup(id=1))
+    instance.object_set.count.return_value = 1
+
     form = ObjectGroupForm(
         data={
-            "add_type": ObjectGroupAddChoices.SINGLE_OBJECT.value[0],
             "label": "",
             "materials": "wood,stone",
             "dating": "XIXe",
         },
-        instance=ObjectGroup(id=1),
+        instance=instance,
     )
-    assert form.fields["add_type"].disabled
+    assert not form.fields["add_type"].required
+    assert form.fields["add_type"].widget.attrs["disabled"]
+    assert not form.has_error("add_type")
+
+
+def test_update_object_set_add_type_depending_on_object_count():
+    single_object_instance = MagicMock(side_effect=ObjectGroup(id=1))
+    single_object_instance.object_set.count.return_value = 1
+    multi_object_instance = MagicMock(side_effect=ObjectGroup(id=2))
+    multi_object_instance.object_set.count.return_value = 3
+
+    single_object_form = ObjectGroupForm(
+        data={
+            "label": "",
+            "materials": "wood,stone",
+            "dating": "XIXe",
+        },
+        instance=single_object_instance,
+    )
+    multi_object_form = ObjectGroupForm(
+        data={
+            "label": "",
+            "materials": "wood,stone",
+            "dating": "XIXe",
+        },
+        instance=multi_object_instance,
+    )
+    assert (
+        single_object_form.fields["add_type"].initial
+        == ObjectGroupAddChoices.SINGLE_OBJECT.value[0]
+    )
+    assert (
+        multi_object_form.fields["add_type"].initial
+        == ObjectGroupAddChoices.OBJECT_GROUP.value[0]
+    )
