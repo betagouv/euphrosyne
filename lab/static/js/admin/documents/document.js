b5dfcb6bf4f11a1f4879cea770f3e2a873b5ab4e
witold <witoldwrob@gmail.com>
2021-12-23 15:12:07 +0100

    adds actions cell with buttons

--- lab/static/js/admin/documents/document.js
+++ lab/static/js/admin/documents/document.js
@@ -21,9 +21,14 @@
         sizeEl.innerText = formatBytes(
           parseInt(documentEl.querySelector("Size").textContent || "0")
         );
+        const actionsEl = document.createElement("td");
+        actionsEl.appendChild(
+          createActionCell(documentEl.querySelector("Key").textContent)
+        );
         rowEl.appendChild(keyEl);
         rowEl.appendChild(lastModifiedEl);
         rowEl.appendChild(sizeEl);
+        rowEl.appendChild(actionsEl);
         tableBodyEl.appendChild(rowEl);
       });
     } else {
@@ -58,19 +63,12 @@
     request.send();
   }
 
-  function formatBytes(a, b = 2, k = 1024) {
-    /**
-     * Format bytes int size.
-     * Taken from this SO answer : https://stackoverflow.com/a/18650828/7433420
-     */
-    with (Math) {
-      let d = floor(log(a) / log(k));
-      return 0 == a
-        ? "0 Bytes"
-        : parseFloat((a / pow(k, d)).toFixed(max(0, b))) +
-            " " +
-            ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][d];
-    }
+  function onDownloadButtonClick(event) {
+    const key = event.target.dataset.key;
+  }
+
+  function onDeleteButtonClick(event) {
+    const key = event.target.dataset.key;
   }
 
   function toggleLoading(active) {
@@ -91,6 +89,41 @@
     }
   }
 
+  function createActionCell(key) {
+    const actionsEl = new DOMParser().parseFromString(
+      '<ul class="fr-btns-group fr-btns-group--inline fr-btns-group--sm">\
+    <li><button class="download-btn fr-btn fr-fi-download-line fr-btn--secondary"></button></li>\
+    <li><button class="delete-btn fr-btn fr-fi-delete-line fr-btn--secondary"></button></li>\
+    </ul>',
+      "text/html"
+    );
+    const buttons = actionsEl.querySelectorAll("button");
+    buttons[0].textContent = gettext("Download file");
+    buttons[0].setAttribute("title", gettext("Download file"));
+    buttons[0].setAttribute("data-key", key);
+    buttons[0].addEventListener("click", onDownloadButtonClick);
+    buttons[1].textContent = gettext("Delete file");
+    buttons[1].setAttribute("title", gettext("Delete file"));
+    buttons[1].setAttribute("title", gettext("Download file"));
+    buttons[1].setAttribute("data-key", key);
+    buttons[1].addEventListener("download", onDeleteButtonClick);
+    return actionsEl.body.firstElementChild;
+  }
+
+  function formatBytes(a, b = 2, k = 1024) {
+    /**
+     * Format bytes int size.
+     * Taken from this SO answer : https://stackoverflow.com/a/18650828/7433420
+     */
+    with (Math) {
+      let d = floor(log(a) / log(k));
+      return 0 == a
+        ? "0 Bytes"
+        : parseFloat((a / pow(k, d)).toFixed(max(0, b))) +
+            " " +
+            ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][d];
+    }
+  }
   window.addEventListener("DOMContentLoaded", (event) => {
     toggleLoading(true);
     fetchDocuments(projectId);
