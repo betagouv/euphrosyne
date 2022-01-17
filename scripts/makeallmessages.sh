4f213e412c7a8d823b88f731610686b1a3627090
lajarre <alexandre.hajjar@gmail.com>
2022-01-17 18:19:06 +0100

    💻 Improve scripts

--- scripts/makeallmessages.sh
+++ scripts/makeallmessages.sh
@@ -3,4 +3,5 @@ set -e
 SCRIPTS_DIR="$(dirname "$0")"
 PROJECT_DIR="$(dirname $SCRIPTS_DIR)"
 
-python "$PROJECT_DIR"/manage.py makemessages -v 0 -a --no-location --no-obsolete
+python "$PROJECT_DIR"/manage.py makemessages --all --verbosity 0 --no-location --no-obsolete
+python "$PROJECT_DIR"/manage.py makemessages --all --verbosity 0 --no-location --no-obsolete -d djangojs
