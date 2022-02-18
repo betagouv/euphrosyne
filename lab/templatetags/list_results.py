b050cc53e334292d04f7d60ab26cf0deb85cf80a
lajarre <alexandre.hajjar@gmail.com>
2022-02-18 01:14:52 +0000

    simplify

--- lab/templatetags/list_results.py
+++ lab/templatetags/list_results.py
@@ -1,7 +1,3 @@
-import html
-import re
-import unicodedata
-
 from django import template
 from django.contrib.admin.templatetags.admin_list import result_list
 from django.contrib.admin.templatetags.base import InclusionAdminNode
@@ -14,44 +10,6 @@ from ..models.project import ProjectStatus
 register = template.Library()
 
 
-def strip_accents(text):
-    return "".join(
-        c for c in unicodedata.normalize("NFD", text) if unicodedata.category(c) != "Mn"
-    )
-
-
-def get_class_name(text):
-    return strip_accents(text).replace(" ", "-").lower()
-
-
-class PrettyStatusNode(template.Node):
-    def __init__(self, nodelist):
-        self.nodelist = nodelist
-
-    def render(self, context):
-        output = self.nodelist.render(context)
-        output_unescaped = html.unescape(output)
-        if 'class="field-status"' in output_unescaped:
-            return re.sub(
-                r">(.*?)<",
-                lambda m: (
-                    fr'><span class="fr-tag {get_class_name(m.group(1))}">'
-                    fr"{m.group(1)}"
-                    "</span><"
-                ),
-                output_unescaped,
-            )
-
-        return output
-
-
-@register.tag
-def prettify(parser, _):
-    nodelist = parser.parse(("endprettify",))
-    parser.delete_first_token()
-    return PrettyStatusNode(nodelist)
-
-
 def custom_result_list(changelist):
     result_list_dict = result_list(changelist)
     _results = result_list_dict["results"]
@@ -62,7 +20,7 @@ def custom_result_list(changelist):
         choice_identifier = ProjectStatus.names[
             ProjectStatus.values.index(project.status)
         ]
-        class_name = get_class_name(choice_identifier)
+        class_name = choice_identifier.lower()
         display = display_for_field(1, project._meta.get_field("status"), "")
         result[list_display_status_index] = format_html(
             '<td class="field-status">'
