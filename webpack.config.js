5f523d5f2de920def8a09b2458f70706c7388cd1
Maxime Cattet <maximecattet@gmx.com>
2022-03-14 12:49:14 +0100

    ➕Add remixicon to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,8 @@ export default {
     main: [
       "promise-polyfill/src/polyfill",
       "whatwg-fetch",
-      "./euphrosyne/assets/js/main.js",
-      "@gouvfr/dsfr/dist/core/core.module.min.js",
       "@gouvfr/dsfr/dist/dsfr/dsfr.min.css",
+      "remixicon/fonts/remixicon.css",
       "./euphrosyne/assets/css/base.css",
     ],
     ...Object.assign(
