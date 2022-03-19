78caa8acf1e89cfa6d34337365ebfe548ca79654
witold <witoldwrob@gmail.com>
2022-03-19 17:34:02 +0100

    🔨 updates create branch db script DB_PORT env var is used if provided

--- scripts/create_branch_db.sh
+++ scripts/create_branch_db.sh
@@ -5,5 +5,5 @@ PROJECT_DIR="$(dirname $SCRIPTS_DIR)"
 
 source "$PROJECT_DIR"/.env
 branch_db_name="$DB_NAME""_""$($SCRIPTS_DIR/db_suffix_for_branch.sh)"
-psql postgres --host $DB_HOST --user $DB_USER -c "CREATE DATABASE $branch_db_name with template $DB_NAME;"
+psql postgres --host $DB_HOST --user $DB_USER --port ${DB_PORT:-5432} -c "CREATE DATABASE $branch_db_name with template $DB_NAME;"
 echo "🐣 Created new database $branch_db_name from template $DB_NAME."
