f20e122c158c490e1cc03c2ec347254d06e7e93f
Maxime Cattet <maximecattet@gmx.com>
2022-03-21 17:13:37 +0100

    ⏪️Revert makeallmessages

--- scripts/makeallmessages.sh
+++ scripts/makeallmessages.sh
@@ -3,5 +3,5 @@ set -e
 SCRIPTS_DIR="$(dirname "$0")"
 PROJECT_DIR="$(dirname $SCRIPTS_DIR)"
 
-pipenv run "$PROJECT_DIR"/manage.py makemessages --all --verbosity 0 --no-location --no-obsolete
-pipenv run "$PROJECT_DIR"/manage.py makemessages --all --verbosity 0 --no-location --no-obsolete -d djangojs
+python "$PROJECT_DIR"/manage.py makemessages --all --verbosity 0 --no-location --no-obsolete
+python "$PROJECT_DIR"/manage.py makemessages --all --verbosity 0 --no-location --no-obsolete -d djangojs
