fd238de84e5beb2290c7673172581cd35a217f2c
Maxime Cattet <maximecattet@gmx.com>
2022-03-22 11:55:40 +0100

    🐛Fix propagation of buttons & links inside run card
    The issue was when clicking on edit or delete on a run card, the card was triggering a selecting event, thus showing a split second (while loading the new page) the footer action.
    By stopping the propagation we avoid that

--- lab/assets/js/pages/run.js
+++ lab/assets/js/pages/run.js
@@ -16,11 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   getDeleteRunButtons().forEach((el) => {
     const runid = el.dataset.runid;
-    el.addEventListener("click", () => handleDelete(runid));
+    el.addEventListener("click", (e) => {
+      handleDelete(runid);
+      // Stop the propagation to avoid triggering the card selection event
+      e.stopPropagation();
+    });
   });
 
   getCards().forEach((el) => {
     el.addEventListener("click", () => toggleRunSelection(el));
+
+    // Stop the propagation to avoid triggering the card selection event
+    el.querySelectorAll("a").forEach((linkEl) => {
+      linkEl.addEventListener("click", (e) => {
+        e.stopPropagation();
+      });
+    });
   });
 
   getRunChangeStateActionButton()?.addEventListener(
