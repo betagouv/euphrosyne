992cfd90a288ea51e7293e1b9ad04408a3b7dab5
witold <witoldwrob@gmail.com>
2022-05-09 12:00:38 +0200

    adds projects to jwt payload

--- euphro_auth/jwt/tests/test_serializers.py
+++ euphro_auth/jwt/tests/test_serializers.py
@@ -1,17 +1,25 @@
-from django.contrib.auth import get_user_model
 from django.middleware.csrf import get_token
-from django.test import RequestFactory
+from django.test import RequestFactory, TestCase
+from rest_framework_simplejwt.state import token_backend
+
+from lab.tests.factories import ParticipationFactory
 
 from ..serializers import SessionTokenObtainSerializer
 
 
-def test_validate_return_tokens():
-    request = RequestFactory().post("/api/auth/token/")
-    request.COOKIES["csrftoken"] = get_token(request)
-    request.user = get_user_model()
-    serializer = SessionTokenObtainSerializer()
-    serializer.context["request"] = request
+class TestSerializers(TestCase):
+    @staticmethod
+    def test_validate_return_tokens():
+        participation = ParticipationFactory()
+        request = RequestFactory().post("/api/auth/token/")
+        request.COOKIES["csrftoken"] = get_token(request)
+        request.user = participation.user
+        serializer = SessionTokenObtainSerializer()
+        serializer.context["request"] = request
 
-    data = serializer.validate(None)
-    assert "refresh" in data
-    assert "access" in data
+        data = serializer.validate(None)
+        assert "refresh" in data
+        assert "access" in data
+        decoded_token = token_backend.decode(data["access"])
+        assert "projects" in decoded_token
+        assert decoded_token["projects"][0]["id"] == participation.project.id
