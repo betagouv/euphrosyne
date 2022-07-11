bcb86395eca15dc77aee8941738bbe5fc1322e43
witold <witoldwrob@gmail.com>
2022-07-11 15:32:07 +0200

    🔥 removes python s3 related code

--- euphrosyne/api_urls.py
+++ euphrosyne/api_urls.py
@@ -4,6 +4,4 @@ app_name = "api"
 
 urlpatterns = [
     path("auth/", include("euphro_auth.api_urls")),
-    path("", include("lab.documents.api_urls")),
-    path("", include("lab.workplace.api_urls")),
 ]
