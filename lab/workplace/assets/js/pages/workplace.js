2ae5cc0ce1dfe30c8c4a0f5a1c663413e8d51c57
witold <witoldwrob@gmail.com>
2022-07-11 15:17:20 +0200

    adds virtual office feature

--- lab/workplace/assets/js/pages/workplace.js
+++ lab/workplace/assets/js/pages/workplace.js
@@ -6,9 +6,10 @@ import { FileTable } from "../../../../assets/js/components/file-table.js";
 import { FileUploadForm } from "../../../../assets/js/components/file-upload-form.js";
 import { initRawData } from "../raw-data/index.js";
 import { initProcessedData } from "../processed-data/index.js";
-
+import { VirtualOfficeButton } from "../components/virtual-office-button.js";
 FileTable.init();
 FileUploadForm.init();
+VirtualOfficeButton.init();
 
 initRawData();
 initProcessedData();
