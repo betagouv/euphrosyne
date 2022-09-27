73a9c8d453c25109edcbfe0589f8e17504b4705e
witold <witoldwrob@gmail.com>
2022-09-27 10:57:50 +0200

    [bugfix] fix lazy trans json error

--- lab/templatetags/inlines.py
+++ lab/templatetags/inlines.py
@@ -1,13 +1,15 @@
 import json
 
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 
 @register.filter
-def update_inline_formset_data_add_text(inline_formset_data: str, value: str):
+@stringfilter
+def update_inline_formset_data_add_text(inline_formset_data: str, value):
     data = json.loads(inline_formset_data)
     if "options" in data:
-        data["options"]["addText"] = value
+        data["options"]["addText"] = str(value)
     return json.dumps(data)
