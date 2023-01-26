b0938ab83d2051fdec1d719df303d3f8eba612b7
witold <witoldwrob@gmail.com>
2023-01-26 16:32:14 +0100

    ✨ Add HDF5 links to webapp

---
+++ lab/workplace/assets/js/hdf5/hdf5-file-table.js
@@ -0,0 +1,35 @@
+import { FileTable } from "../../../../assets/js/components/file-table.js";
+
+export class HDF5FileTable extends FileTable {
+  constructor() {
+    super();
+    this.classList.add("file-table");
+    this.dataRows = [];
+  }
+
+  static init() {
+    customElements.define("hdf5-file-table", HDF5FileTable, {
+      extends: "table",
+    });
+  }
+
+  generateActionCellContent(file) {
+    const { path } = file;
+    const unorderedList = document.createElement("ul");
+    unorderedList.classList.add(
+      "fr-btns-group",
+      "fr-btns-group--inline",
+      "fr-btns-group--sm"
+    );
+    const viewLink = document.createElement("a");
+    viewLink.textContent = window.gettext("View");
+    viewLink.href = `${this.getAttribute("h5webapp-host")}/?file=${path}`;
+    viewLink.target = "_blank";
+    viewLink.rel = "noopener";
+    const anchorListItem = document.createElement("li");
+    anchorListItem.appendChild(viewLink);
+    unorderedList.appendChild(anchorListItem);
+
+    return unorderedList;
+  }
+}
