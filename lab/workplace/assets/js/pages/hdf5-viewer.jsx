459b3bee10511bd2a4ec30f2592509671f06d024
witold <witoldwrob@gmail.com>
2023-07-06 17:50:51 +0200

    feat(hdf5-viewer): add token to axios requests

--- lab/workplace/assets/js/pages/hdf5-viewer.jsx
+++ lab/workplace/assets/js/pages/hdf5-viewer.jsx
@@ -3,22 +3,54 @@ import { BrowserRouter } from "react-router-dom";
 import { App, H5GroveProvider } from "@h5web/app";
 import { useLocation } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-
+import { getToken } from "../../../../assets/js/jwt";
 import "@h5web/app/styles.css";
+
+function createAxiosResponseInterceptor(axiosInstance) {
+  // When response code is 401, try to refresh the token. Eject the interceptor so it doesn't loop in case
+  // token refresh causes the 401 response. Must be re-attached later on or the token refresh will only happen once
+  // Taken from : https://stackoverflow.com/a/53294310/7433420
+  const interceptor = axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      // Reject promise if usual error
+      if (error.response.status !== 401) {
+        return Promise.reject(error);
+      }
+      axiosInstance.interceptors.response.eject(interceptor);
+      return getToken(false)
+        .then((token) => {
+          error.response.config.headers["Authorization"] = "Bearer " + token;
+          // Retry the initial call, but with the updated token in the headers.
+          // Resolves the promise if successful
+          return axiosInstance(error.response.config);
+        })
+        .finally(() => createAxiosResponseInterceptor(axiosInstance)); // Re-attach the interceptor by running the method
+    }
+  );
+}
 
 function HDF5ViewerApp() {
   const search = useLocation().search;
   const file = new URLSearchParams(search).get("file");
 
-  return (
-    <H5GroveProvider
-      url="http://localhost:8001/hdf5"
-      filepath={file}
-      axiosConfig={{ params: { file, projectName: window.projectName } }}
-    >
-      <App explorerOpen={false} />
-    </H5GroveProvider>
-  );
+  const h5webApp = App({ explorerOpen: false });
+
+  const h5GroveProvider = H5GroveProvider({
+    url: `${process.env.EUPHROSYNE_TOOLS_API_URL}/hdf5`,
+    filepath: file,
+    axiosConfig: { params: { file } },
+    children: h5webApp,
+  });
+
+  h5GroveProvider.props.api.client.interceptors.request.use(async (config) => {
+    const token = await getToken();
+    config.headers["Authorization"] = "Bearer " + token;
+    return config;
+  });
+  createAxiosResponseInterceptor(h5GroveProvider.props.api.client);
+
+  return h5GroveProvider;
 }
 
 const container = document.getElementById("root");
