3b18a42890581e8591522b2100cd68b7890b01e7
Maxime Cattet <maximecattet@gmx.com>
2023-07-13 11:10:27 +0200

    feat: Use the new url

--- lab/workplace/assets/js/raw-data/raw-data-file-service.js
+++ lab/workplace/assets/js/raw-data/raw-data-file-service.js
@@ -5,7 +5,7 @@ import { FileService } from "../../../../assets/js/file-service.js";
 export class RawDataFileService extends FileService {
   constructor(projectSlug, runName) {
     super(
-      `/data/${projectSlug}/runs/${runName}/raw_data/folders`,
+      `/data/${projectSlug}/runs/${runName}/raw_data`,
       `/data/runs/shared_access_signature`
     );
   }
