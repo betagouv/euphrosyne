2e2ed85c7f4a7921d3faf8a7ae2c196d7246f284
witold <witoldwrob@gmail.com>
2024-01-25 16:41:42 +0100

    chore: add react & ts support

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,11 @@ export default {
         use: ["source-map-loader"],
       },
       {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
