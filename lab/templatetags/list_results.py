9dda085e7a55c741dba456a7922afa08d7132642
witold <witoldwrob@gmail.com>
2024-02-07 19:32:55 +0100

    polissage: project changelist page
    feat(polissage): page changelist ui

--- lab/templatetags/list_results.py
+++ lab/templatetags/list_results.py
@@ -1,45 +1,72 @@
+from itertools import groupby
+from typing import Any, Callable
+
 from django import template
-from django.contrib.admin.templatetags.admin_list import result_list
+from django.contrib.admin.templatetags.admin_list import (
+    ResultList,
+    items_for_result,
+    result_list,
+)
 from django.contrib.admin.templatetags.base import InclusionAdminNode
+from django.contrib.admin.views.main import ChangeList
+from django.db.models.functions import TruncMonth
+from django.db.models.query import QuerySet
 from django.utils.html import format_html
+
+from lab.models.project import ProjectQuerySet
 
 from ..models import Project
 
 register = template.Library()
 
 
-def project_results(
-    list_display_status_index: int,
-    _result_list: list[Project],
-    results: list[list[str]],
-):
-    def update_result(project: Project, result: list[str]) -> list[str]:
-        project_status = project.status
-        choice_identifier = project_status.name
-        class_name = choice_identifier.lower()
-        result[list_display_status_index] = format_html(
-            '<td class="field-status">'
-            f'<span class="fr-tag fr-tag--sm {class_name}">'
-            f"{project_status.value[1]}"
-            "</span>"
-            "</td>"
-        )
-        return result
-
-    return [
-        update_result(_project, _result)
-        for _project, _result in zip(_result_list, results)
-    ]
-
-
-def project_result_list(changelist):
-    result_list_dict = result_list(changelist)
-    result_list_dict["results"] = project_results(
-        changelist.list_display.index("status"),
-        changelist.result_list,
-        result_list_dict["results"],
+def _get_status_cell(project: Project) -> str:
+    project_status = project.status
+    choice_identifier = project_status.name
+    class_name = choice_identifier.lower()
+    return format_html(
+        '<td class="field-status">'
+        f'<span class="fr-tag fr-tag--sm {class_name}">'
+        f"{project_status.value[1]}"
+        "</span>"
+        "</td>"
     )
-    return result_list_dict
+
+
+def _get_admin_cell(project: Project) -> str:
+    admin_text = project.admin.get_full_name() if project.admin else ""
+    return format_html('<td class="field-admin">' f"{admin_text}" "</td>")
+
+
+def _get_leader_cell(project: Project) -> str:
+    leader_text = project.leader.user.get_full_name() if project.leader else ""
+    return format_html('<td class="field-leader">' f"{leader_text}" "</td>")
+
+
+def update_result(
+    changelist: ChangeList, project: Project, result: list[str]
+) -> list[str]:
+    status_index = changelist.list_display.index("status")
+    admin_index = changelist.list_display.index("admin")
+    leader_index = changelist.list_display.index("leader_user")
+
+    result[status_index] = _get_status_cell(project)
+    result[admin_index] = _get_admin_cell(project)
+    result[leader_index] = _get_leader_cell(project)
+    return result
+
+
+def project_results(
+    changelist: ChangeList,
+    results: list[list[str]],
+    project_list_results: list[Project] | ProjectQuerySet | None = None,
+):
+    if not project_list_results:
+        project_list_results = changelist.result_list
+    return [
+        update_result(changelist, _project, _result)
+        for _project, _result in zip(project_list_results, results)
+    ]
 
 
 @register.tag(name="project_result_list")
@@ -51,3 +78,95 @@ def project_result_list_tag(parser, token):
         template_name="change_list_results.html",
         takes_context=False,
     )
+
+
+def project_result_list(
+    changelist: ChangeList, queryset: QuerySet = None, regroup: bool = False
+):
+    """
+    Updates the result list of the given ChangeList object
+    based on the provided queryset or pagination settings.
+
+    Args:
+        changelist (ChangeList): The ChangeList object representing the list of results.
+        queryset (QuerySet, optional): The queryset to be used as the result list.
+            Defaults to None.
+        regroup (bool, optional): Flag indicating whether to regroup the results.
+            Defaults to False.
+
+    Returns:
+        dict: A dictionary containing the updated result list to be used in template.
+    """
+    if queryset:
+        changelist.result_list = queryset
+    else:
+        not_paginated = (
+            changelist.show_all and changelist.can_show_all
+        ) or not changelist.multi_page
+        if not_paginated:
+            changelist.result_list = (
+                changelist.queryset._clone()  # pylint: disable=protected-access
+            )
+        else:
+            changelist.result_list = changelist.paginator.page(
+                changelist.page_num
+            ).object_list
+
+    result_list_dict = result_list(changelist)
+    result_list_dict["results"] = project_results(
+        changelist=changelist,
+        results=result_list_dict["results"],
+    )
+    if regroup:
+        result_list_dict["do_regroup"] = regroup
+        result_list_dict["grouped_results"] = group_results_by_month(changelist)
+    return result_list_dict
+
+
+def group_results_by_month(changelist: ChangeList):
+    changelist.result_list = changelist.result_list.annotate(
+        month=TruncMonth("first_run_date")
+    )
+    return _group_results(changelist, attr_getter_fn=lambda p: p.month)
+
+
+def _group_results(changelist: ChangeList, attr_getter_fn: Callable[[Project], Any]):
+    """
+    Group the results from a ChangeList based on a specific attribute.
+
+    This function sorts the results in the ChangeList in descending order
+    based on the attribute specified by attr_getter_fn.
+    It then groups the results by this attribute and yields each group along with
+    a list of ResultList objects for the results in that group.
+
+    Args:
+        changelist (ChangeList): The ChangeList to group.
+        attr_getter_fn (str): The function to use to get the
+            attribute to sort & group by.
+
+    Yields:
+        tuple: A tuple containing the group and a list of ResultList objects
+            for that group.
+    """
+    grouped_results = groupby(
+        sorted(changelist.result_list, key=attr_getter_fn, reverse=True),
+        attr_getter_fn,
+    )
+
+    for group, result_grouper in grouped_results:
+        results = list(result_grouper)
+        yield (
+            group,
+            project_results(
+                changelist,
+                results=[
+                    update_result(
+                        changelist,
+                        result,
+                        ResultList(None, items_for_result(changelist, result, None)),
+                    )
+                    for result in results
+                ],
+                project_list_results=results,
+            ),
+        )
--- lab/tests/managers/test_project_manager.py
+++ lab/tests/managers/test_project_manager.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 
 from ...models import Project
-from ..factories import ProjectFactory
+from ..factories import ProjectFactory, RunFactory
 
 
 class ProjectModelTestCase(TestCase):
@@ -23,3 +23,107 @@ class ProjectModelTestCase(TestCase):
         Project.objects.create(name="Public Project", confidential=False)
         Project.objects.create(name="Confidential Project", confidential=True)
         self.assertEqual(Project.objects.only_public().count(), 1)
+
+    def test_filter_by_status(self):
+        to_schedule_ids = [
+            r.project.id for r in RunFactory.create_batch(5, start_date=None)
+        ]
+        scheduled_ids = [
+            r.project.id
+            for r in RunFactory.create_batch(
+                5, start_date=timezone.now() + timezone.timedelta(days=1)
+            )
+        ]
+        ongoing_ids = [
+            r.project.id
+            for r in RunFactory.create_batch(
+                5,
+                start_date=timezone.now() - timezone.timedelta(days=1),
+                end_date=timezone.now() + timezone.timedelta(days=1),
+            )
+        ]
+        finished_ids = [
+            r.project.id
+            for r in RunFactory.create_batch(
+                5,
+                start_date=timezone.now() - timezone.timedelta(days=2),
+                end_date=timezone.now() - timezone.timedelta(days=1),
+                project__is_data_available=False,
+            )
+        ]
+        data_available_ids = [
+            r.project.id
+            for r in RunFactory.create_batch(
+                5,
+                start_date=timezone.now() - timezone.timedelta(days=2),
+                end_date=timezone.now() - timezone.timedelta(days=1),
+                project__is_data_available=True,
+            )
+        ]
+
+        self.assertListEqual(
+            list(
+                Project.objects.filter_by_status(
+                    Project.Status.TO_SCHEDULE
+                ).values_list("id", flat=True)
+            ),
+            to_schedule_ids,
+        )
+        self.assertListEqual(
+            list(
+                Project.objects.filter_by_status(Project.Status.SCHEDULED).values_list(
+                    "id", flat=True
+                )
+            ),
+            scheduled_ids,
+        )
+        self.assertListEqual(
+            list(
+                Project.objects.filter_by_status(Project.Status.ONGOING).values_list(
+                    "id", flat=True
+                )
+            ),
+            ongoing_ids,
+        )
+        self.assertListEqual(
+            list(
+                Project.objects.filter_by_status(Project.Status.FINISHED).values_list(
+                    "id", flat=True
+                )
+            ),
+            finished_ids,
+        )
+        self.assertListEqual(
+            list(
+                Project.objects.filter_by_status(
+                    Project.Status.DATA_AVAILABLE
+                ).values_list("id", flat=True)
+            ),
+            data_available_ids,
+        )
+
+    def test_annotate_first_run_date(self):
+        project = ProjectFactory()
+        first_run = RunFactory(
+            start_date=timezone.now() + timezone.timedelta(days=1), project=project
+        )
+        RunFactory(
+            start_date=timezone.now() + timezone.timedelta(days=2),
+            project=project,
+        )  # later run to test
+
+        queried_project = Project.objects.annotate_first_run_date().get(id=project.id)
+
+        assert hasattr(queried_project, "first_run_date")
+        assert queried_project.first_run_date == first_run.start_date
+
+    def test_only_to_schedule(self):
+        # pylint: disable=expression-not-assigned
+        RunFactory(
+            start_date=timezone.now() + timezone.timedelta(days=1),
+        ).project
+        to_schedule_projects = [ProjectFactory(), RunFactory(start_date=None).project]
+        assert Project.objects.only_to_schedule().count() == 2
+        self.assertListEqual(
+            list(Project.objects.only_to_schedule()), to_schedule_projects
+        )
