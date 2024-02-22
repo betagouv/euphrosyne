60f3d3dd1e48a0e679716744bc787cf53855c0d5
witold <witoldwrob@gmail.com>
2024-02-22 16:48:52 +0100

    polissage run changeform

--- lab/admin/run.py
+++ lab/admin/run.py
@@ -1,7 +1,6 @@
-from datetime import time
-from typing import Any, List, Optional, Tuple, Type, Union
+from typing import Any, List, Optional, Tuple, Type
 
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.admin import ModelAdmin
 from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.admin.views.main import ChangeList
@@ -9,15 +8,23 @@ from django.core.exceptions import PermissionDenied
 from django.db.models.query import QuerySet
 from django.forms import ModelForm
 from django.http.request import HttpRequest
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from euphro_tools.hooks import initialize_run_directory, rename_run_directory
 
-from ..forms import RunDetailsForm
+from ..forms import (
+    RunCreateAdminForm,
+    RunCreateForm,
+    RunDetailsAdminForm,
+    RunDetailsForm,
+    RunScheduleForm,
+)
 from ..models import Project, Run
 from ..objects.fields import ObjectGroupChoiceField
 from ..permissions import LabRole, is_lab_admin
-from ..widgets import RelatedObjectRunWidgetWrapper, SplitDateTimeWithDefaultTime
+from ..widgets import RelatedObjectRunWidgetWrapper
 from .mixins import LabPermission, LabPermissionMixin
 from .run_actions import change_state
 
@@ -88,7 +95,6 @@ class RunChangeList(ChangeList):
 @admin.register(Run)
 class RunAdmin(LabPermissionMixin, ModelAdmin):
     class Media:
-        js = ("pages/run.js",)
         css = {
             "all": (
                 "css/admin/methods.css",
@@ -97,33 +103,9 @@ class RunAdmin(LabPermissionMixin, ModelAdmin):
         }
 
     HIDE_ADD_SIDEBAR = True
-    form = RunDetailsForm
     actions = [change_state]
     list_filter = ("project",)
-    fieldsets = (
-        (_("Project"), {"fields": ("project",)}),
-        (
-            _("Basic information"),
-            {"fields": ("label", "status", "start_date", "end_date")},
-        ),
-        (
-            _("Experimental conditions"),
-            {
-                "fields": ("particle_type", "energy_in_keV", "beamline"),
-                "classes": ("mb-0",),
-            },
-        ),
-        (
-            "METHODS",
-            {
-                "fields": (
-                    *[f.name for f in Run.get_method_fields()],
-                    *[f.name for f in Run.get_detector_fields()],
-                    *[f.name for f in Run.get_filters_fields()],
-                )
-            },
-        ),
-    )
+
     lab_permissions = LabPermission(
         add_permission=LabRole.ANY_STAFF_USER,
         view_permission=LabRole.PROJECT_MEMBER,
@@ -153,18 +135,76 @@ class RunAdmin(LabPermissionMixin, ModelAdmin):
     def has_module_permission(self, request: HttpRequest) -> bool:
         return False
 
-    def get_readonly_fields(
-        self, request: HttpRequest, obj: Optional[Run] = None
-    ) -> Union[List[str], Tuple]:
-        readonly_fields = (*super().get_readonly_fields(request, obj), "status")
+    def get_form(
+        self,
+        request: HttpRequest,
+        obj: Run | None = None,
+        change: bool = False,
+        **kwargs
+    ):
+        if is_lab_admin(request.user):
+            if obj:
+                return RunDetailsAdminForm
+            return RunCreateAdminForm
         if obj:
-            # Prevent changing label after creation (because of data folder sync).
-            readonly_fields += ("project",)
+            return RunDetailsForm
+        return RunCreateForm
 
-        if not is_lab_admin(request.user):
-            readonly_fields += ("start_date", "end_date")
+    def get_fieldsets(
+        self, request: HttpRequest, obj: Optional[Run] = None
+    ) -> list[Tuple[Optional[str], dict[str, Any]]]:
+        fieldset_classes = [
+            "fr-mb-0",
+            "fr-pb-1w",
+            "flex-container",
+            "flex-flow--wrap",
+        ]
+        fieldsets = []
 
-        return readonly_fields
+        member_basic_info_fields = ["label"]
+        admin_basic_info_fields = [*member_basic_info_fields, "embargo_date"]
+        if not obj:
+            fieldsets += [
+                (
+                    _("Basic information"),
+                    {
+                        "fields": (
+                            admin_basic_info_fields
+                            if is_lab_admin(request.user)
+                            else member_basic_info_fields
+                        )
+                    },
+                )
+            ]
+        else:
+            if is_lab_admin(request.user):
+                fieldsets += [
+                    (
+                        _("Basic information"),
+                        {"fields": ("label",), "classes": [*fieldset_classes]},
+                    )
+                ]
+            fieldsets += [
+                (
+                    _("Experimental conditions"),
+                    {
+                        "fields": ("particle_type", "energy_in_keV", "beamline"),
+                        "classes": [*fieldset_classes],
+                    },
+                ),
+                (
+                    "METHODS",
+                    {
+                        "fields": (
+                            *[f.name for f in Run.get_method_fields()],
+                            *[f.name for f in Run.get_detector_fields()],
+                            *[f.name for f in Run.get_filters_fields()],
+                        )
+                    },
+                ),
+            ]
+
+        return fieldsets
 
     def get_changelist(self, request, **kwargs):
         return RunChangeList
@@ -192,18 +232,6 @@ class RunAdmin(LabPermissionMixin, ModelAdmin):
             return runs_queryset
         return runs_queryset.filter(project__members__id=request.user.id).distinct()
 
-    def formfield_for_dbfield(  # pylint: disable=arguments-differ
-        self, db_field, request: HttpRequest, **kwargs
-    ):
-        # Widget edit here because form fields are overriden by admin
-        # & SplitDateTimeWithDefaultTime is an admin widget and cannot be used directly
-        # in a ModelForm (it returns a 2-n list as value instead of a 1-n list).
-        if db_field.name == "start_date":
-            kwargs["widget"] = SplitDateTimeWithDefaultTime(default_time_value=time(9))
-        if db_field.name == "end_date":
-            kwargs["widget"] = SplitDateTimeWithDefaultTime(default_time_value=time(18))
-        return super().formfield_for_dbfield(db_field, request, **kwargs)
-
     def formfield_for_foreignkey(  # pylint: disable=arguments-differ
         self, db_field, request: HttpRequest, queryset: QuerySet[Run] = None, **kwargs
     ):
@@ -219,19 +247,43 @@ class RunAdmin(LabPermissionMixin, ModelAdmin):
 
     def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
         project = self._get_project(request, object_id)
+        if not project:
+            self.message_user(
+                request,
+                _(
+                    "You do not have permission for this project. "
+                    "If you think this is an error, please contact the New AGLAE team."
+                ),
+                messages.WARNING,
+            )
+            return redirect(reverse("admin:lab_project_changelist"))
         is_popup = "_popup" in request.GET
-        return super().changeform_view(
-            request,
-            object_id,
-            form_url,
-            {
-                **(extra_context if extra_context else {}),
-                "show_save_as_new": False,
-                "show_save_and_add_another": False,
-                "show_save": is_popup,
-                "project": project,
-            },
-        )
+        extra_context = {
+            **(extra_context if extra_context else {}),
+            "show_save_as_new": False,
+            "show_save_and_add_another": False,
+            "show_save": is_popup,
+            "project": project,
+        }
+
+        if request.method == "POST" and request.POST.get("_action") == "schedule_run":
+            schedule_form = self._schedule_action(
+                request,
+                obj=self.get_object(request, object_id),
+            )
+            request.method = "GET"  # treat as a GET request to display the form again
+        else:
+            schedule_form = RunScheduleForm(
+                instance=self.get_object(request, object_id),
+            )
+
+        if object_id is not None:
+            extra_context["schedule_modal"] = {
+                "id": "schedule-modal",
+                "form": schedule_form,
+            }
+
+        return super().changeform_view(request, object_id, form_url, extra_context)
 
     def changelist_view(
         self, request: HttpRequest, extra_context: Optional[dict[str, str]] = None
@@ -245,6 +297,11 @@ class RunAdmin(LabPermissionMixin, ModelAdmin):
             },
         )
 
+    def save_form(self, request: HttpRequest, form: ModelForm, change: bool):
+        if not change:
+            form.instance.project = self._get_project(request, form.instance.id)
+        return super().save_form(request, form, change)
+
     def save_model(self, request: Any, obj: Run, form: ModelForm, change: bool) -> None:
         super().save_model(request, obj, form, change)
         if not change:
@@ -254,7 +311,26 @@ class RunAdmin(LabPermissionMixin, ModelAdmin):
 
     def _get_project(self, request, object_id=None) -> Optional[Project]:
         if object_id:
-            return self.get_object(request, object_id).project
+            run = self.get_object(request, object_id)
+            if not run:
+                return None
+            return run.project
         if "project" in request.GET:
-            return Project.objects.get(id=request.GET["project"])
+            qs = Project.objects.filter(id=request.GET["project"])
+            if not is_lab_admin(request.user):
+                qs = qs.filter(members__id=request.user.id)
+            return qs.first()
         return None
+
+    def _schedule_action(self, request: HttpRequest, obj: Run) -> None:
+        schedule_form = RunScheduleForm(
+            data=request.POST,
+            files=request.FILES,
+            instance=obj,
+        )
+        if not is_lab_admin(request.user):
+            raise PermissionDenied
+        if schedule_form.is_valid():
+            new_object = self.save_form(request, schedule_form, change=True)
+            self.save_model(request, new_object, schedule_form, True)
+        return schedule_form
--- lab/tests/admin/test_run_admin.py
+++ lab/tests/admin/test_run_admin.py
@@ -7,9 +7,11 @@ from django.core.exceptions import PermissionDenied
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.urls import reverse
+from django.utils import timezone
 
 from lab.models.project import Project
 
+from ... import forms
 from ...admin import RunAdmin
 from ...models import Run
 from .. import factories
@@ -63,6 +65,7 @@ class TestRunAdminPermissions(TestCase):
 
 class TestRunAdminParams(TestCase):
     def setUp(self):
+        self.add_url = reverse("admin:lab_run_add")
         self.run_admin = RunAdmin(model=Run, admin_site=AdminSite())
         self.project = factories.ProjectWithLeaderFactory()
         self.leader_user = self.project.leader.user
@@ -76,30 +79,28 @@ class TestRunAdminParams(TestCase):
             reverse("admin:lab_run_change", args=[self.run.id])
         )
         request.user = self.lab_admin_user
-        assert "project" in self.run_admin.get_readonly_fields(request, self.run)
+        form = self.run_admin.get_form(request, obj=self.run, change=True)
+        assert "project" not in form().fields
 
-    def test_dates_are_readonly_when_change_as_non_admin(self):
-        request = RequestFactory().get(
-            reverse("admin:lab_run_change", args=[self.run.id])
-        )
-        request.user = self.leader_user
-        assert "start_date" in self.run_admin.get_readonly_fields(request, self.run)
-        assert "end_date" in self.run_admin.get_readonly_fields(request, self.run)
-
-        request.user = self.lab_admin_user
-        assert "start_date" not in self.run_admin.get_readonly_fields(request, self.run)
-        assert "end_date" not in self.run_admin.get_readonly_fields(request, self.run)
-
-    def test_dates_are_readonly_when_add_as_non_admin(self):
+    def test_get_form_when_create(self):
         request = RequestFactory().get(reverse("admin:lab_run_add"))
 
         request.user = self.leader_user
-        assert "start_date" in self.run_admin.get_readonly_fields(request)
-        assert "end_date" in self.run_admin.get_readonly_fields(request)
+        assert isinstance(
+            self.run_admin.get_form(request, obj=None, change=False)(),
+            forms.RunCreateForm,
+        )
 
-        request.user = self.lab_admin_user
-        assert "start_date" not in self.run_admin.get_readonly_fields(request)
-        assert "end_date" not in self.run_admin.get_readonly_fields(request)
+    def test_get_form_when_change(self):
+        request = RequestFactory().get(
+            reverse("admin:lab_run_change", args=[self.run.id])
+        )
+
+        request.user = self.leader_user
+        assert isinstance(
+            self.run_admin.get_form(request, obj=None, change=False)(),
+            forms.RunCreateForm,
+        )
 
     def test_get_queryset_exludes_non_member_projects(self):
         request = RequestFactory().get(
@@ -133,9 +134,9 @@ class TestRunAdminParams(TestCase):
 
     def test_project_when_adding_from_project(self):
         project = factories.ProjectFactory()
-        add_url = reverse("admin:lab_run_add")
-        request = RequestFactory().get(f"{add_url}?project={project.id}")
-        request.user = factories.StaffUserFactory()
+        request = RequestFactory().get(f"{self.add_url}?project={project.id}")
+        user = request.user = factories.StaffUserFactory()
+        project.members.add(user)
         # pylint: disable=protected-access
         assert self.run_admin._get_project(request) == project
 
@@ -144,6 +145,20 @@ class TestRunAdminParams(TestCase):
         request.user = factories.StaffUserFactory()
         # pylint: disable=protected-access
         assert self.run_admin._get_project(request) is None
+
+    def test_project_is_none_when_not_a_member(self):
+        project = factories.ProjectFactory()
+        request = RequestFactory().get(f"{self.add_url}?project={project.id}")
+        request.user = factories.StaffUserFactory()
+        # pylint: disable=protected-access
+        assert self.run_admin._get_project(request) is None
+
+    def test_project_when_admin(self):
+        project = factories.ProjectFactory()
+        request = RequestFactory().get(f"{self.add_url}?project={project.id}")
+        request.user = factories.LabAdminUserFactory()
+        # pylint: disable=protected-access
+        assert self.run_admin._get_project(request) == project
 
 
 class TestRunAdminViewAsLeader(TestCase):
@@ -154,20 +169,6 @@ class TestRunAdminViewAsLeader(TestCase):
         self.project = factories.ProjectFactory()
         self.project.participation_set.create(
             user=self.project_leader_user, is_leader=True
-        )
-
-    def test_add_run_of_non_lead_project_not_allowed(self):
-        "Test add Run of non-lead Project not allowed via formfield_for_foreignkey"
-        request = self.request_factory.get(reverse("admin:lab_run_add"))
-        request.user = self.staff_user
-        admin = RunAdmin(Run, admin_site=AdminSite())
-        admin_form = admin.get_form(request, obj=None, change=False)
-
-        form = admin_form(data={"project": self.project.id})
-        assert len(form.errors["project"]) == 1
-        assert (
-            "Ce choix ne fait pas partie de ceux disponibles."
-            in form.errors["project"][0]
         )
 
     @patch("lab.admin.run.initialize_run_directory")
@@ -184,47 +185,6 @@ class TestRunAdminViewAsLeader(TestCase):
                 change=False,
             )
         init_run_dir_mock.assert_called_once_with(run.project.name, run.label)
-
-    @patch("lab.admin.run.rename_run_directory")
-    def test_change_run_label_calls_hook(self, rename_run_dir_mock):
-        run = factories.RunFactory()
-        run.project.members.add(self.staff_user)
-        request = self.request_factory.post(
-            reverse(
-                "admin:lab_run_change",
-                args=[run.id],
-            ),
-            data={
-                "label": "new-run-name",
-                "particle_type": "",
-                "energy_in_keV_Proton": "",
-                "energy_in_keV_Alpha particle": "",
-                "energy_in_keV_Deuton": "",
-                "beamline": "Microbeam",
-                "filters_for_detector_LE0": "",
-                "filters_for_detector_HE1": "",
-                "filters_for_detector_HE2": "",
-                "filters_for_detector_HE3": "",
-                "filters_for_detector_HE4": "",
-                "detector_IBIL_other": "",
-                "detector_FORS_other": "",
-                "detector_ERDA": "",
-                "detector_NRA": "",
-                "Run_run_object_groups-TOTAL_FORMS": "0",
-                "Run_run_object_groups-INITIAL_FORMS": "0",
-            },
-        )
-        request.user = self.staff_user
-        with patch.object(run, "save"):
-            run_admin = RunAdmin(Run, admin_site=AdminSite())
-            form_class = run_admin.get_form(request, obj=run, change=True)
-            form = form_class(request.POST, request.FILES, instance=run)
-            # Clean form to populate run instance
-            form.is_valid()
-            run_admin.save_model(request, run, form=form, change=True)
-        rename_run_dir_mock.assert_called_once_with(
-            run.project.name, form.initial["label"], "new-run-name"
-        )
 
     def test_change_list_view_with_project_lookup_when_project_member(self):
         """Calling changelist_view with a project member should return ok"""
@@ -244,6 +204,21 @@ class TestRunAdminViewAsLeader(TestCase):
         admin = RunAdmin(Run, admin_site=AdminSite())
         with pytest.raises(PermissionDenied):
             admin.changelist_view(request)
+
+    @patch.object(RunAdmin, "message_user", MagicMock())
+    def test_add_redirect_when_no_project_membership(self):
+        """Calling add_view with a non-project member should redirect"""
+        request = self.request_factory.post(
+            reverse("admin:lab_run_add") + f"?project={self.project.id}",
+            data={
+                "label": "Run 1",
+            },
+        )
+        request.user = self.staff_user
+        admin = RunAdmin(Run, admin_site=AdminSite())
+        response = admin.changeform_view(request, None)
+
+        assert response.status_code == 302
 
 
 class TestRunAdminViewAsAdmin(TestCase):
@@ -291,16 +266,61 @@ class TestRunAdminViewAsAdmin(TestCase):
         admin = RunAdmin(Run, admin_site=AdminSite())
         assert admin.changelist_view(request)
 
+    @patch("lab.admin.run.rename_run_directory")
+    def test_change_run_label_calls_hook(self, rename_run_dir_mock):
+        run = factories.RunFactory()
+        request = self.request_factory.post(
+            reverse(
+                "admin:lab_run_change",
+                args=[run.id],
+            ),
+            data={
+                "label": "new-run-name",
+                "particle_type": "",
+                "energy_in_keV_Proton": "",
+                "energy_in_keV_Alpha particle": "",
+                "energy_in_keV_Deuton": "",
+                "beamline": "Microbeam",
+                "filters_for_detector_LE0": "",
+                "filters_for_detector_HE1": "",
+                "filters_for_detector_HE2": "",
+                "filters_for_detector_HE3": "",
+                "filters_for_detector_HE4": "",
+                "detector_IBIL_other": "",
+                "detector_FORS_other": "",
+                "detector_ERDA": "",
+                "detector_NRA": "",
+                "Run_run_object_groups-TOTAL_FORMS": "0",
+                "Run_run_object_groups-INITIAL_FORMS": "0",
+            },
+        )
+        request.user = self.admin_user
+        with patch.object(run, "save"):
+            run_admin = RunAdmin(Run, admin_site=AdminSite())
+            form_class = run_admin.get_form(request, obj=run, change=True)
+            form = form_class(request.POST, request.FILES, instance=run)
+            # Clean form to populate run instance
+            assert form.is_valid()
+            run_admin.save_model(request, run, form=form, change=True)
+        rename_run_dir_mock.assert_called_once_with(
+            run.project.name, form.initial["label"], "new-run-name"
+        )
+
 
 class TestRunAdminMethodFieldset(TestCase):
     def setUp(self):
+        self.project = factories.ProjectFactory()
+        self.run = factories.RunFactory(project=self.project)
         self.request = RequestFactory().get(
             reverse(
-                "admin:lab_run_add",
+                "admin:lab_run_change",
+                args=[self.run.id],
             )
+            + f"?project={self.project.id}"
         )
         self.admin_user = factories.LabAdminUserFactory()
         self.request.user = self.admin_user
+        self.request.resolver_match = MagicMock()
         self.run_admin = RunAdmin(Run, admin_site=AdminSite())
         self.admin_form = self.run_admin.get_form(self.request, obj=None, change=False)
 
@@ -308,11 +328,94 @@ class TestRunAdminMethodFieldset(TestCase):
         assert "METHODS" in [
             fieldset_name
             for fieldset_name, fieldset_options in self.run_admin.get_fieldsets(
-                self.request
+                self.request, self.run
             )
         ]
 
     def test_methods_fieldset_is_rendered(self):
-        resp = self.run_admin.add_view(self.request)
+        resp = self.run_admin.change_view(self.request, str(self.run.id))
         resp.render()
         assert '<fieldset id="METHODS"' in resp.content.decode()
+
+
+# pylint: disable=too-many-instance-attributes
+class TestRunAdminScheduleAction(TestCase):
+    def setUp(self):
+        self.run = factories.RunFactory()
+        self.project = self.run.project
+        self.admin_user = factories.LabAdminUserFactory()
+        self.run_admin = RunAdmin(Run, admin_site=AdminSite())
+
+        now = timezone.now()
+        self.start_date = now + timezone.timedelta(days=14)
+        self.end_date = now + timezone.timedelta(days=15)
+        self.embargo_date = now + timezone.timedelta(days=365 * 2)
+        self.correct_data = {
+            "_action": "schedule_run",
+            "_continue": "1",
+            "start_date_0": self.start_date.date().strftime("%Y-%m-%d"),
+            "start_date_1": self.start_date.time().strftime("%H:%M:%S"),
+            "end_date_0": self.end_date.date().strftime("%Y-%m-%d"),
+            "end_date_1": self.end_date.time().strftime("%H:%M:%S"),
+            "embargo_date": self.embargo_date.date().strftime("%Y-%m-%d"),
+        }
+
+    def test_schedule_form_is_rendered(self):
+        request = RequestFactory().get(
+            reverse(
+                "admin:lab_run_change",
+                args=[self.run.id],
+            )
+            + f"?project={self.project.id}"
+        )
+        request.user = self.admin_user
+        request.resolver_match = MagicMock()
+
+        resp = self.run_admin.change_view(request, str(self.run.id))
+        resp.render()
+
+        assert "schedule_modal" in resp.context_data
+        assert resp.context_data["schedule_modal"]["id"] == "schedule-modal"
+        assert isinstance(
+            resp.context_data["schedule_modal"]["form"], forms.RunScheduleForm
+        )
+        assert '<form id="schedule-modal-form"' in resp.content.decode()
+
+    def test_schedule_action_as_admin(self):
+        request = RequestFactory().post(
+            reverse(
+                "admin:lab_run_change",
+                args=[self.run.id],
+            ),
+            data=self.correct_data,
+        )
+        request.user = self.admin_user
+
+        with timezone.override("UTC"):
+            response = self.run_admin.changeform_view(request, str(self.run.id))
+
+        run = Run.objects.get(pk=self.run.id)
+        assert response.status_code == 200
+        assert self.start_date.strftime("%Y-%m-%d %H:%M:%S") == run.start_date.strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        assert self.end_date.strftime("%Y-%m-%d %H:%M:%S") == run.end_date.strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        assert self.embargo_date.strftime("%Y-%m-%d") == run.embargo_date.strftime(
+            "%Y-%m-%d"
+        )
+
+    def test_schedule_action_as_non_admin(self):
+        request = RequestFactory().post(
+            reverse(
+                "admin:lab_run_change",
+                args=[self.run.id],
+            ),
+            data=self.correct_data,
+        )
+        request.user = factories.StaffUserFactory()
+        self.run.project.members.add(request.user)
+
+        with pytest.raises(PermissionDenied):
+            self.run_admin.changeform_view(request, str(self.run.id))
