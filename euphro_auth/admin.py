c9e854ccaa978234b5c38e1e41f75072e9443761
witold <witoldwrob@gmail.com>
2024-03-06 15:16:55 +0100

    polissage: user pages

--- euphro_auth/admin.py
+++ euphro_auth/admin.py
@@ -7,9 +7,13 @@ from django.db.models.query import QuerySet
 from django.forms.models import ModelForm
 from django.http import HttpResponse
 from django.http.request import HttpRequest
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
 
+from lab.models.participation import Participation
 from lab.permissions import is_lab_admin
 
 from .emails import send_invitation_email
@@ -17,21 +21,89 @@ from .forms import UserChangeForm, UserCreationForm
 from .models import User, UserInvitation
 
 
+class ProjectInline(admin.TabularInline):
+    model = Participation
+    verbose_name = _("Related project")
+    verbose_name_plural = _("Related projects")
+    extra = 0
+
+    fields = ("project", "status_display")
+    readonly_fields = ("project", "status_display")
+
+    def has_view_permission(
+        self, request: HttpRequest, obj: User | None = None
+    ) -> bool:
+        return is_lab_admin(request.user) or request.user == obj
+
+    def has_change_permission(
+        self, request: HttpRequest, obj: User | None = None
+    ) -> bool:
+        return False
+
+    def has_delete_permission(
+        self, request: HttpRequest, obj: User | None = None
+    ) -> bool:
+        return False
+
+    def has_add_permission(self, request: HttpRequest, obj: User | None = None) -> bool:
+        return False
+
+    def has_view_or_change_permission(
+        self, request: HttpRequest, obj: User | None = None
+    ) -> bool:
+        if not obj:
+            return True
+        return is_lab_admin(request.user) or request.user == obj
+
+    @admin.display(description=_("Status"))
+    def status_display(self, obj: Participation) -> str:
+        return format_html(
+            '<p class="fr-tag {}">{}</p>',
+            obj.project.status.name.lower(),
+            obj.project.status.value[1],
+        )
+
+
+class InvitationCompletedStatusListFilter(admin.SimpleListFilter):
+    title = _("Invitation completed")
+    template = "admin/lab/project/filter.html"
+    parameter_name = "invitation_completed"
+
+    def lookups(self, request, model_admin):
+        return [
+            (True, _("Yes")),
+            (False, _("No")),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            if self.value() == "True":
+                return queryset.filter(
+                    invitation_completed_at__isnull=False,
+                )
+            if self.value() == "False":
+                return queryset.filter(
+                    invitation_completed_at__isnull=True,
+                )
+        return queryset
+
+
 class UserAdmin(DjangoUserAdmin):
-    add_form = UserCreationForm
-    add_form_template = "euphro_add_form.html"
-    form = UserChangeForm
+    class Media:
+        css = {"all": ("css/admin/user.css",)}
+
     model = User
-    list_display = ("email", "is_staff", "is_active", "is_lab_admin")
-    list_filter = ("email", "is_staff", "is_active")
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Profile"), {"fields": ("first_name", "last_name")}),
-        (
-            "Permissions",
-            {"fields": ("is_staff", "is_lab_admin", "is_active")},
-        ),
-    )
+
+    add_form_template = "euphro_add_form.html"
+
+    add_form = UserCreationForm
+    form = UserChangeForm
+
+    list_filter = (InvitationCompletedStatusListFilter,)
+    list_per_page = 30
+    show_facets = False
+    actions = ("send_invitation_mail_action",)
+
     add_fieldsets = (
         (
             None,
@@ -41,24 +113,30 @@ class UserAdmin(DjangoUserAdmin):
             },
         ),
     )
-    search_fields = ("email",)
+    search_fields = ("email", "first_name", "last_name")
     ordering = (
         "last_name",
         "first_name",
         "email",
     )
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    readonly_fields = ("password_display",)
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs.prefetch_related("groups")
 
     def get_list_display(self, request):
+        list_display = (
+            "email",
+            "full_name_display",
+            "last_institution_display",
+            "invitation_completed_display",
+            "is_lab_admin",
+        )
         if request.user.is_superuser:
-            return self.list_display + ("is_superuser",)
-        return self.list_display
+            return list_display + ("is_superuser_display",)
+        return list_display
 
     def get_readonly_fields(self, request: HttpRequest, obj: Optional[User] = None):
         if request.user.is_superuser:
@@ -68,48 +146,93 @@ class UserAdmin(DjangoUserAdmin):
     def get_fieldsets(
         self, request: HttpRequest, obj: Optional[User] = None
     ) -> List[Tuple[Optional[str], Dict[str, Any]]]:
-        fieldsets = super().get_fieldsets(request, obj)
+        fieldset_classes = [
+            "fr-mb-0",
+            "fr-pb-1w",
+        ]
+        fieldsets = []
+        fieldsets += [
+            (
+                _("Profile"),
+                {
+                    "fields": (
+                        ("email", "password_display"),
+                        "first_name",
+                        "last_name",
+                    ),
+                    "classes": [*fieldset_classes],
+                },
+            ),
+        ]
+        if is_lab_admin(request.user):
+            fieldsets += [
+                (
+                    _("Permissions"),
+                    {
+                        "fields": ("is_staff", "is_lab_admin", "is_active"),
+                        "classes": [*fieldset_classes],
+                    },
+                ),
+            ]
         if request.user.is_superuser:
-            return fieldsets + (
+            fieldsets += [
                 (
                     gettext("Superuser"),
-                    {"fields": ("is_superuser",)},
+                    {"fields": ("is_superuser",), "classes": [*fieldset_classes]},
                 ),
-            )
+            ]
         return fieldsets
+
+    def get_inlines(self, request: HttpRequest, obj: Optional[User] = None):
+        if obj:
+            return [ProjectInline]
+        return []
+
+    def changelist_view(
+        self, request: HttpRequest, extra_context: Optional[Dict[str, str]] = None
+    ):
+        return super().changelist_view(
+            request,
+            {
+                **(extra_context if extra_context else {}),
+                "title": _("Users"),
+            },
+        )
+
+    def changeform_view(
+        self,
+        request: HttpRequest,
+        object_id: str,
+        form_url: str = "",
+        extra_context: dict[str, str] | None = None,
+    ):  # pylint: disable=signature-differs
+        return super().changeform_view(
+            request,
+            object_id,
+            form_url,
+            {
+                **(extra_context if extra_context else {}),
+                "show_save_and_add_another": False,
+                "show_save_as_new": False,
+            },
+        )
 
     def has_view_permission(
         self, request: HttpRequest, obj: Optional[User] = None
     ) -> bool:
-        return request.user.is_staff and is_lab_admin(request.user)
+        return request.user.is_staff and (
+            is_lab_admin(request.user) or request.user == obj
+        )
 
     def has_change_permission(
         self, request: HttpRequest, obj: Optional[User] = None
     ) -> bool:
-        return request.user.is_staff and is_lab_admin(request.user)
+        return request.user.is_staff and (
+            is_lab_admin(request.user) or request.user == obj
+        )
 
-
-class UserInvitationAdmin(ModelAdmin):
-    list_display = ("email", "invitation_completed", "invitation_completed_at")
-    fields = ("email",)
-    actions = ("send_invitation_mail_action",)
-
-    def add_view(
-        self,
-        request: HttpRequest,
-        form_url: str = "",
-        extra_context: dict[str, Any] = None,
-    ) -> HttpResponse:
-        extra_context = {"title": _("Invite new user")}
-        return super().add_view(request, form_url, extra_context)
-
-    @admin.display(
-        description=_("Invitation completed"),
-        boolean=True,
-        ordering="invitation_completed_at",
-    )
-    def invitation_completed(self, instance: UserInvitation):
-        return instance.invitation_completed_at is not None
+    def has_delete_permission(self, request, obj=None):
+        return False
 
     @admin.action(description=_("Send invitation e-mail again"))
     def send_invitation_mail_action(
@@ -139,6 +262,61 @@ class UserInvitationAdmin(ModelAdmin):
                 message,
                 messages.WARNING,
             )
+
+    @admin.display(description=_("Name"))
+    def full_name_display(self, obj: User) -> str:
+        return obj.get_full_name()
+
+    @admin.display(description=_("Last institution"))
+    def last_institution_display(self, obj: User) -> str:
+        last_participation = obj.participation_set.order_by("-created").first()
+        if last_participation and last_participation.institution:
+            return str(last_participation.institution)
+        return ""
+
+    @admin.display(description=_("Invitation completed"), boolean=True)
+    def invitation_completed_display(self, obj: User) -> bool:
+        return obj.invitation_completed_at is not None
+
+    @admin.display(description="Super-user", boolean=True)
+    def is_superuser_display(self, obj: User):
+        return obj.is_superuser
+
+    @admin.display(description=_("Password"))
+    def password_display(self, obj: User):
+        change_pasword_link = format_html(
+            '<a href="{}">{}</a>',
+            reverse("admin:auth_user_password_change", args=[obj.pk]),
+            _("this form"),
+        )
+        text = (
+            _("Change password using {}.")
+            if obj.password
+            else _("No password. You can set one using {}.")
+        ).format(change_pasword_link)
+        return format_html('<div class="help">{}</div>', mark_safe(text))
+
+
+class UserInvitationAdmin(ModelAdmin):
+    list_display = ("email", "invitation_completed", "invitation_completed_at")
+    fields = ("email", "first_name", "last_name")
+
+    def add_view(
+        self,
+        request: HttpRequest,
+        form_url: str = "",
+        extra_context: dict[str, Any] = None,
+    ) -> HttpResponse:
+        extra_context = {"title": _("Invite new user")}
+        return super().add_view(request, form_url, extra_context)
+
+    @admin.display(
+        description=_("Invitation completed"),
+        boolean=True,
+        ordering="invitation_completed_at",
+    )
+    def invitation_completed(self, instance: UserInvitation):
+        return instance.invitation_completed_at is not None
 
     def save_model(
         self,
