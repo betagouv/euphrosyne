200ea1c97f39b5365e138e6f340e2e4e4118dd1e
witold <witoldwrob@gmail.com>
2024-05-02 12:14:15 +0200

    refactor: move models __init__ to models.py module

--- lab/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from ..objects import Location, Object, ObjectGroup, Period  # noqa: F401
-from ..participations.models import Institution, Participation  # noqa: F401
-from ..projects.models import BeamTimeRequest, Project  # noqa: F401
-from ..runs.models import Run  # noqa: F401
