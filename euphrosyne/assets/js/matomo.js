c0c141fb3808d45596f0dda0ffdef416eccf17ab
witold <witoldwrob@gmail.com>
2024-09-13 11:58:36 +0200

    feat: add matomo tag manager

--- euphrosyne/assets/js/matomo.js
+++ euphrosyne/assets/js/matomo.js
@@ -13,3 +13,15 @@ _paq.push(["enableLinkTracking"]);
   g.src = u + "piwik.js";
   s.parentNode.insertBefore(g, s);
 })();
+
+// Matomo Tag Manager
+var _mtm = (window._mtm = window._mtm || []);
+_mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+(function () {
+  var d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0];
+  g.async = true;
+  g.src = "https://stats.beta.gouv.fr/js/container_RtFSe5hb.js";
+  s.parentNode.insertBefore(g, s);
+})();
