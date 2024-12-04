9ff4fe3935f8eee39274411c9c070f6206a2fba5
Witold <dev@witold.fr>
2024-12-04 12:09:02 +0100

    feat: update cron job to use new script for indexing and building catalog

---
+++ scripts/scalingo_index_and_build_catalog.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Command to be run in Scalingo one-off container
+# Index elasticsearch catalog & build digilab catalog
+python manage.py index_elasticsearch_catalog
+install-scalingo-cli
+scalingo -a $SCALINGO_EUPHROSYNE_RELATED_DIGILAB_APP --region $REGION_NAME integration-link-manual-deploy main
