458df058da53f16d0436c114e2b31e3265fb2ac9
Witold <dev@witold.fr>
2025-01-10 16:01:17 +0100

    fix: set default Content-Type header in euphrosyneToolsFetch

--- shared/js/euphrosyne-tools-client.ts
+++ shared/js/euphrosyne-tools-client.ts
@@ -36,6 +36,7 @@ export default async function euphrosyneToolsFetch(
   return _fetch(`${baseURL}${input}`, {
     ...init,
     headers: {
+      "Content-Type": "application/json",
       ...init?.headers,
       Authorization: `Bearer ${getAccessToken()}`,
     },
