6fa6a5b0555a9005716741521e277c78a7c8ad1e
Witold <dev@witold.fr>
2025-03-07 16:28:15 +0100

    fix(blob-service): remove blobHTTPHeaders from uploadFile function

--- lab/assets/js/blob-service.ts
+++ lab/assets/js/blob-service.ts
@@ -5,9 +5,7 @@ import { BlockBlobClient } from "@azure/storage-blob";
 export async function uploadFile(file: File, url: string) {
   await new BlockBlobClient(url)
     .getBlockBlobClient()
-    .uploadData(await file.arrayBuffer(), {
-      blobHTTPHeaders: { blobCacheControl: "max-age=3400" },
-    });
+    .uploadData(await file.arrayBuffer());
 
   return { file };
 }
