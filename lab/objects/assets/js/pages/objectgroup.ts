b4fc16b051068244c62b36447ea74967cc12470b
Witold <dev@witold.fr>
2025-10-21 11:40:47 +0200

    feat(objectgroup): move page to typescript

--- lab/objects/assets/js/pages/objectgroup.ts
+++ lab/objects/assets/js/pages/objectgroup.ts
@@ -9,18 +9,20 @@ import {
 } from "../object/form.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const getObjectCountInput = () => document.getElementById("id_object_count");
+  const getObjectCountInput = () =>
+    document.getElementById("id_object_count") as HTMLInputElement;
 
   const getAreObjectsDifferentiated = () =>
     document
       .querySelector('button[aria-controls="differentiation-accordion"]')
-      .getAttribute("aria-expanded") === "true";
+      ?.getAttribute("aria-expanded") === "true";
 
   function onFormsetChange() {
-    if (!getAreObjectsDifferentiated()) {
-      getObjectCountInput().value = document.querySelectorAll(
-        "#object_set-group tbody tr.dynamic-object_set",
-      ).length;
+    const objectCountInput = getObjectCountInput();
+    if (!getAreObjectsDifferentiated() && objectCountInput) {
+      objectCountInput.value = document
+        .querySelectorAll("#object_set-group tbody tr.dynamic-object_set")
+        .length.toString();
     }
   }
 
@@ -28,15 +30,18 @@ document.addEventListener("DOMContentLoaded", function () {
     .getElementById("objectgroup_form")
     ?.addEventListener("submit", function () {
       // Disabled object inline formset if objects are not differentiated
-      if (!getAreObjectsDifferentiated()) {
-        document.getElementById("id_object_set-TOTAL_FORMS").value = "0";
+      const totalFormsInput = document.getElementById(
+        "id_object_set-TOTAL_FORMS",
+      ) as HTMLInputElement | null;
+      if (!getAreObjectsDifferentiated() && totalFormsInput) {
+        totalFormsInput.value = "0";
       }
     });
 
   document
     .getElementById("id_add_type")
     ?.addEventListener("change", function (event) {
-      const { value } = event.target;
+      const { value } = event.target as HTMLSelectElement;
       if (value === "SINGLE_OBJECT") {
         displaySingleObjectForm();
       } else if (value === "OBJECT_GROUP") {
@@ -47,17 +52,21 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("id_object_count")
     ?.addEventListener("input", function (event) {
-      if (document.getElementById("id_add_type").value === "SINGLE_OBJECT") {
+      const addTypeInput = document.getElementById(
+        "id_add_type",
+      ) as HTMLInputElement;
+      if (addTypeInput.value === "SINGLE_OBJECT") {
         return;
       }
-      updateObjectRows(event.target.value);
+      updateObjectRows((event.target as HTMLInputElement).value);
     });
 
   document
     .querySelector('button[aria-controls="differentiation-accordion"]')
-    .addEventListener("click", function (event) {
+    ?.addEventListener("click", function (event) {
       const isExpanded = !(
-        event.target.getAttribute("aria-expanded") === "true"
+        (event.target as HTMLButtonElement).getAttribute("aria-expanded") ===
+        "true"
       );
       handleAccordionClick(isExpanded, getObjectCountInput().value);
     });
@@ -65,21 +74,27 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("id_inventory")
     ?.addEventListener("change", (event) => {
-      toggleInlineInputsDisabledOnParentChange("inventory", event.target.value);
+      toggleInlineInputsDisabledOnParentChange(
+        "inventory",
+        (event.target as HTMLInputElement).value,
+      );
     });
   document
     .getElementById("id_collection")
-    .addEventListener("change", (event) => {
+    ?.addEventListener("change", (event) => {
       toggleInlineInputsDisabledOnParentChange(
         "collection",
-        event.target.value,
+        (event.target as HTMLInputElement).value,
       );
     });
 
-  django.jQuery(document).on("formset:added", onFormsetChange);
-  django.jQuery(document).on("formset:removed", onFormsetChange);
+  document.addEventListener("formset:added", onFormsetChange);
+  document.addEventListener("formset:removed", onFormsetChange);
 
-  if (document.getElementById("id_object_count")?.value === "1") {
+  if (
+    (document.getElementById("id_object_count") as HTMLInputElement | null)
+      ?.value === "1"
+  ) {
     displaySingleObjectForm();
   } else {
     displayObjectGroupForm();
