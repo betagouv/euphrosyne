ff5e29b14e1d919b3cb748595f6824fb71e67da9
Witold <dev@witold.fr>
2026-01-13 12:01:38 +0100

    doc(tally): add documentation about tally webhook workflow

--- radiation_protection/constants.py
+++ radiation_protection/constants.py
@@ -1,2 +1,5 @@
+# Constants related to radiation protection certifications
+# When updating name, also update in Tally quiz webhook integration settings
+# in the header Euphrosyne-Certification value.
 RADIATION_PROTECTION_CERTIFICATION_NAME = "AGLAE radioprotection"
 RADIATION_PROTECTION_CERTIFICATION_NUM_DAYS_VALID = 365
