6f660b1d79c35e97be9cefa491ae0165c6e0a41d
Witold <dev@witold.fr>
2026-01-28 14:23:17 +0100

    feat(run-data): implement command to compute run size and file count (#1701)
    * feat(run-data): implement command to compute run size and file count for finished runs
    
    * fix(run-data): improve error handling in run data listing and size computation
    
    * refactor(run-data): remove deprecated management commands and update tests
    
    * feat(download-urls): update URL generation to use urlencode for parameters
    
    * refactor: run black
    
    * refactor(utils): move import statement for EuphroToolsAPIToken inside the function
    
    * refactor(utils): replace direct token access with helper function for mocking in tests

--- euphro_tools/tests/test_download_urls.py
+++ euphro_tools/tests/test_download_urls.py
@@ -16,9 +16,7 @@ class TestDownloaldUrls(TestCase):
         self.requests_get_mock = patcher.start()
         self.addCleanup(patcher.stop)
 
-        patcher = mock.patch(
-            "euphro_tools.download_urls.EuphroToolsAPIToken.for_euphrosyne"
-        )
+        patcher = mock.patch("euphro_tools.utils._get_euphrosyne_token")
         self.token_mock = patcher.start()
         self.addCleanup(patcher.stop)
 
@@ -31,14 +29,14 @@ class TestDownloaldUrls(TestCase):
         patcher.start()
         self.addCleanup(patcher.stop)
 
-        self.token_mock.return_value.access_token = "access"
+        self.token_mock.return_value = "access"
 
     def test_generate_download_url(self):
         url = generate_download_url("project_slug", "run_label", "raw_data", "token")
         assert (
             url
             # pylint: disable=line-too-long
-            == "http://example.com/data/run-data-zip?token=token&path=projects/project_slug/runs/run_label/raw_data"
+            == "http://example.com/data/run-data-zip?token=token&path=projects%2Fproject_slug%2Fruns%2Frun_label%2Fraw_data"
         )
 
     def test_fetch_token_for_run_data(self):
@@ -50,10 +48,13 @@ class TestDownloaldUrls(TestCase):
         )
 
         assert token == "token"
-        assert self.requests_get_mock.call_args[0][0] == (
-            # pylint: disable=line-too-long
-            "http://example.com/data/project_slug/token?path=projects/project_slug/runs/run_label/raw_data"
-        )
+        url = self.requests_get_mock.call_args[0][0]
+        assert url == "http://example.com/data/project_slug/token"
+        params = self.requests_get_mock.call_args[1]["params"]
+        assert params == {
+            "path": "projects/project_slug/runs/run_label/raw_data",
+            "expiration": now.isoformat(),
+        }
         assert self.requests_get_mock.call_args[1]["headers"] == {
             "Authorization": "Bearer access"
         }
@@ -63,7 +64,8 @@ class TestDownloaldUrls(TestCase):
         fetch_token_for_run_data(
             "project_slug", "run_label", "raw_data", data_request_id="1"
         )
-        assert "data_request=1" in self.requests_get_mock.call_args[0][0]
+        params = self.requests_get_mock.call_args[1]["params"]
+        assert params["data_request"] == "1"
 
     def test_fetch_token_for_run_data_raise_euphro_tools_exception(
         self,
