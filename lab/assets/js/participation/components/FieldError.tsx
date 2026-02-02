35f69174738df152f04130a66e12b3e42e9e7883
Witold <dev@witold.fr>
2026-02-02 19:27:50 +0100

    feat(participation): add same email validation for employer and display error message

--- lab/assets/js/participation/components/FieldError.tsx
+++ lab/assets/js/participation/components/FieldError.tsx
@@ -16,40 +16,20 @@ export default function FieldError<T>({
 
   if (Array.isArray(fieldErrors)) {
     return (
-      <div
-        className="fr-messages-group"
+      <ErrorMessage
         id={`participation-${String(firstKey)}-messages`}
-        aria-live="polite"
-      >
-        {fieldErrors.map((errorMsg, idx) => (
-          <p
-            className="fr-message fr-message--error"
-            key={`participation-${String(firstKey)}-error-${idx}`}
-          >
-            {errorMsg}
-          </p>
-        ))}
-      </div>
+        errors={fieldErrors}
+      />
     );
   }
 
   if (!secondKey) {
     if ("non_field_errors" in fieldErrors) {
       return (
-        <div
-          className="fr-messages-group"
+        <ErrorMessage
           id={`participation-${String(firstKey)}-messages`}
-          aria-live="polite"
-        >
-          {fieldErrors.non_field_errors.map((errorMsg, idx) => (
-            <p
-              className="fr-message fr-message--error"
-              key={`participation-${String(firstKey)}-non-field-error-${idx}`}
-            >
-              {errorMsg}
-            </p>
-          ))}
-        </div>
+          errors={fieldErrors.non_field_errors}
+        />
       );
     }
     return null;
@@ -66,21 +46,23 @@ export default function FieldError<T>({
 
     if (Array.isArray(secondFieldErrors)) {
       return (
-        <div
-          className="fr-messages-group"
+        <ErrorMessage
           id={`participation-${String(firstKey)}-${String(secondKey)}-messages`}
-          aria-live="polite"
-        >
-          {secondFieldErrors.map((errorMsg, idx) => (
-            <p
-              className="fr-message fr-message--error"
-              key={`participation-${String(firstKey)}-${String(secondKey)}-error-${idx}`}
-            >
-              {errorMsg}
-            </p>
-          ))}
-        </div>
+          errors={secondFieldErrors}
+        />
       );
     }
   }
+}
+
+export function ErrorMessage({ id, errors }: { id: string; errors: string[] }) {
+  return (
+    <div className="fr-messages-group" id={id} aria-live="polite">
+      {errors.map((errorMsg, idx) => (
+        <p className="fr-message fr-message--error" key={`${id}-error-${idx}`}>
+          {errorMsg}
+        </p>
+      ))}
+    </div>
+  );
 }
