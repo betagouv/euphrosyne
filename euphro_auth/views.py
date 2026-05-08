5b1a60246f4f08b959748b0b4bbd20a092e64923
Witold <dev@witold.fr>
2026-05-08 15:32:58 +0200

    chore: add type ignore

--- euphro_auth/views.py
+++ euphro_auth/views.py
@@ -28,7 +28,7 @@ class UserTokenRegistrationView(PasswordResetConfirmView):
 
     def get_context_data(self, **kwargs: Any):
         context = super().get_context_data(**kwargs)
-        context.update({"user_id": self.user.id})
+        context.update({"user_id": self.user.id})  # type: ignore
         return context
 
 
